2e227985a2bbccf793ea44c7a2271a486478b7b8
Andrew Helbig <helbigventures@gmail.com>
2023-12-12 16:47:37 -0800

    added malloc free MD MM MM_st

--- benchmarks/MM_st/bench.c
+++ benchmarks/MM_st/bench.c
@@ -1,55 +1,52 @@
 #include <stdio.h>
-#include <stdlib.h>     /* malloc, free, rand */
-
 #include "util.h"
 
 #define ASIZE 2048
 #define STEP   128
-#define ITERS    2
-#define LEN  32768
+#define ITERS  2
+#define LEN    32768
 
 int arr[ASIZE];
 
-//make sure struct occupies an entire cache line.
 struct ll {
   int val;
-  long long p1,p2,p3,p4,p5,p6,p7,p8,p9,p10;
-  long long q1,q2,q3,q4,q5,q6,q7,q8,q9,q10;
   struct ll* _next;
 };
 
+// Pre-allocate an array of nodes
+struct ll nodes[LEN + 1];
+
 __attribute__ ((noinline))
-int loop(int zero,struct ll* n) {
-  int t = 0,i,iter;
-  for(iter=0; iter < ITERS; ++iter) {
-    struct ll* cur =n;
-    while(cur!=NULL) {
-      //t+=cur->val;
+int loop(int zero, struct ll* n) {
+  int t = 0, i, iter;
+  for(iter = 0; iter < ITERS; ++iter) {
+    struct ll* cur = n;
+    while(cur != NULL) {
+      //t += cur->val;
       cur->val=7; //a tore!
-      cur=cur->_next;
+      cur = cur->_next;
     }
   }
   return t;
 }
 
 int main(int argc, char* argv[]) {
-   argc&=10000;
-   struct ll *n, *cur;
+  argc &= 10000;
+  struct ll* n = nodes;
+  struct ll* cur = n;
 
-   int i;
-   n=malloc(sizeof(struct ll));
-   cur=n;
-   for(i=0;i<LEN;++i) {
-     cur->val=i;
-     cur->_next=malloc(sizeof(struct ll));
-     cur=cur->_next;
-   }
-   cur->val=100;
-   cur->_next=NULL;
+  int i;
+  for (i = 0; i < LEN; ++i) {
+    cur->val = i;
+    cur->_next = cur + 1; // Point to the next element in the array
+    cur = cur->_next;
+  }
+  cur->val = 100;
+  cur->_next = NULL;
 
-   setStats(1);
-   int t=loop(argc,n);
-   setStats(0);
+  setStats(1);
+  int t = loop(argc, n);
+  setStats(0);
 
-   volatile int a = t;
+  volatile int a = t;
 }
